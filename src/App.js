c284b78586a9b3baed70584c939e103f5a966eb7
mdteixeira <matheusdiegoteixeira@gmail.com>
2023-06-05 11:51:17 -0300

    commit 2 05-06

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
-import PrimeiroComponente from './Components/PrimeiroComponente';
-
+import PrimeiroComponente from './Components/component';
+import Componente2 from './Components/componente2';
 let nome = 'Matheus';
 let sobrenome = 'Teixeira';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <h2 className='greeting'>
-                    Seja bem-vindo,{' '}
-                    <span className="nome">
-                        {nome} {sobrenome}
-                    </span>
-                    !
-                </h2>
-            </header>
+                <header className="App-header">
+                    <h2 className="greeting">
+                        Seja bem-vindo,{' '}
+                        <span className="nome">
+                            {nome} {sobrenome}
+                        </span>
+                        !
+                    </h2>
+                </header>
+                <main>
             <PrimeiroComponente />
+            <Componente2 />
+                </main>
         </div>
     );
 }
---
+++ src/Components/component.js
@@ -0,0 +1,10 @@
+const PrimeiroComponente = () => {
+    return (
+        <div>
+            <h2>Meu primeiro componente</h2>
+            <p>Lorem ipsum dolor sit amet. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam praesentium debitis suscipit eveniet quod atque aut sapiente obcaecati eaque consequatur molestiae, vero cum optio eius, maxime, modi dicta! Deserunt, reprehenderit fugiat! Nobis, ducimus eaque. Amet alias a quisquam esse commodi! Necessitatibus assumenda possimus iure, doloribus unde placeat doloremque quos tempora?</p>
+        </div>
+    )
+}
+
+export default PrimeiroComponente;
