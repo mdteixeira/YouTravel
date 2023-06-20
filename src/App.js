04f1060d08507ac9862464e27f0ae76061f55e65
mdteixeira <matheusdiegoteixeira@gmail.com>
2023-06-20 11:00:34 -0300

    commit 20/06

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
+import Input from './Components/input';
 import './App.css';
 import PrimeiroComponente from './Components/component';
-import Componente2 from './Components/componente2';
 let nome = 'Matheus';
 let sobrenome = 'Teixeira';
 
 function App() {
-
-    const handleMsg = () => {
-        return console.log('Cliquei no botão');
-    }
-
     return (
         <div className="App">
             <header className="App-header">
@@ -22,9 +17,8 @@ function App() {
             </header>
             <main>
                 <PrimeiroComponente />
-                <button onClick={handleMsg}>Clique aqui!</button>
-                <Componente2 />
             </main>
+            <Input />
         </div>
     );
 }
